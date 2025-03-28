f68af608e33e2be126b102feb2c976cc899a1196
muskan2205 <muskan.kumari22@s.amity.edu>
2025-03-28 12:52:53 +0530

    case1 is added

---
+++ switch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    printf("Enter a number (1-4): ");
+    scanf("%d", &number);
+
+    switch (number) {
+        case 1:
+            printf("You entered One.\n");
+            break;
