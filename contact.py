5ac6cbae5e623776321f9c865102a62716f889fa
muskan2205 <muskan.kumari22@s.amity.edu>
2025-04-04 13:27:34 +0530

    i created a conatct management system using python (commit 1)

---
+++ contact.py
@@ -0,0 +1,10 @@
+contacts = []
+
+def add_contact():
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    email = input("Enter email: ")
+    contact = {"name": name, "phone": phone, "email": email}
+    contacts.append(contact)
+    print("Contact added successfully!\n")
+
