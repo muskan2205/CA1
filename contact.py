60096f21d12186b015b2765c7c5e2a61ac9b9229
muskan2205 <muskan.kumari22@s.amity.edu>
2025-04-04 13:29:43 +0530

    i created a conatct management system using python (commit 3)

--- contact.py
+++ contact.py
@@ -15,3 +15,13 @@ def view_contacts():
     for idx, contact in enumerate(contacts, start=1):
         print(f"{idx}. Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
     print()
+def search_contact():
+    search_name = input("Enter name to search: ")
+    found = False
+    for contact in contacts:
+        if contact["name"].lower() == search_name.lower():
+            print(f"Found: Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}\n")
+            found = True
+            break
+    if not found:
+        print("Contact not found.\n")
