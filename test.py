eedcdd6659b3905ef1f5b3a5741aaa3646a614cf
muskan2205 <muskan.kumari22@s.amity.edu>
2025-04-15 12:25:02 +0530

    added new code

--- test.py
+++ test.py
@@ -55,3 +55,10 @@ if num % 2 == 0:
 else:
     print(f"{num} is odd")
 
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+largest = max(a, b, c)
+print(f"The largest number is {largest}")
+
