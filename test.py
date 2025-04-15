b938ac69e6ef2026bcf6c1023f96858847102203
muskan2205 <muskan.kumari22@s.amity.edu>
2025-04-15 12:17:29 +0530

    html tags

--- test.py
+++ test.py
@@ -40,3 +40,14 @@ simple_interest = (principal * rate * time) / 100
 
 print("Simple Interest =", simple_interest)
 
+
+
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+sum_result = num1 + num2
+
+print(f"The sum of {num1} and {num2} is {sum_result}")
+
+
