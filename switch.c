eec0b00b0f3a11f34fa5da58d37c5361ac51842a
muskan2205 <muskan.kumari22@s.amity.edu>
2025-05-16 11:48:46 +0530

    new update

--- switch.c
+++ switch.c
@@ -8,16 +8,16 @@ int main() {
 
     switch (number) {
         case 1:
-            printf("You entered 20.\n");
+            printf("You entered 200.\n");
             break;
 	    case 2:
-            printf("You entered 30.\n");
+            printf("You entered 300.\n");
             break;
 	    case 3:
-            printf("You entered 40.\n");
+            printf("You entered 400.\n");
             break;
 	    case 4:
-            printf("You entered 50.\n");
+            printf("You entered 500.\n");
             break;
 	    default:("invalid");
 		    break;
